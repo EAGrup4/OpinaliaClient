a91e300b648d72fbcd587a3438dbd9e211eb2543
Mikelg <mikel.garralaga@estudiant.upc.edu>
2017-12-03 12:14:02 +0100

    update routes

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -37,7 +37,7 @@ export class ProductService {
     const headers = new Headers({ 'Content-Type': 'application/json' });
     const options = new RequestOptions({ headers: headers });
     // ...using get request
-    return this.http.get(this.url + '/' + name, options)
+    return this.http.get(this.url + '/name/' + name, options)
     // ...and calling .json() on the response to return data
       .map((res: Response) => res.json())
       .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
@@ -74,7 +74,7 @@ export class ProductService {
   searchProductByCategory(category: string) {
     const headers = new Headers({ 'Content-Type': 'application/json' });
     const options = new RequestOptions({ headers: headers });
-    return this.http.get(this.url + '/searchProductByCategory/' + category, options)
+    return this.http.get(this.url + '/category/' + category, options)
       .map((res: Response) => res.json())
       .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
   }
