433680bb1ec8040ad062265cd5aea75bcd6950a5
Gurkeerat Singh <gurkeerat.singh.96@gmail.com>
2018-01-20 20:20:27 +0100

    Validator v0.3

--- src/app/Views/yourProfile/yourProfile.component.ts
+++ src/app/Views/yourProfile/yourProfile.component.ts
@@ -26,25 +26,7 @@ export class YourProfileComponent implements OnInit {
     this.user = JSON.parse(this.savedUser);
     console.log(this.user);
     this.imagePrev = this.user.profileImage;
-    this.userService.getTheUser(this.user.name).subscribe(
-      (data) => {
-        console.log(data);
-        this.userIt = data[0];
-        console.log(this.userIt);
-        if (this.userIt.profileImage === '') {
-          this.image = '../../../assets/images/silueta.png';
-        } else {
-          this.image = this.userIt.profileImage;
-
-          let reader = new FileReader();
-
-          reader.onload = (e: any) => {
-            this.image = e.target.result;
-          };
-
-          reader.readAsDataURL(this.userIt.profileImage);
-        }
-      });
+    this.image = this.user.profileImage;
   }
   public closeAlert(alert: IAlert) {
     const index: number = this.alerts.indexOf(alert);
